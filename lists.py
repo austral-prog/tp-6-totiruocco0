99818374ad4a14ab33cdc5f5c0ccfa80dcd5fe59
totiruocco0 <162723302+totiruocco0@users.noreply.github.com>
2024-05-10 13:04:05 -0300

    Update lists.py

--- lists.py
+++ lists.py
@@ -1,20 +1,24 @@
-# Replace the "ANSWER HERE" with your answer
+def remove_elements(removeele):
+    return removeele[1:4] + removeele[7:]
 
-def remove_elements(list_to_remove_elements):
-    return "ANSWER HERE"  # Remove this line and implement
+def add_elements(listadd):
 
-
-def add_elements(list_to_add_elements):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    return ["Pink"] + listadd + ["Yellow"]
 
 def is_empty(list_to_check):
-    return "ANSWER HERE"  # Remove this line and implement
-
+    if list_to_check == []:
+        return True
+    else:
+        return False
 
 def check_lists(list_to_compare1, list_to_compare2):
-    return "ANSWER HERE"  # Remove this line and implement
+    if list_to_compare1[2:3] == [] or list_to_compare2[2:3] == []:
+        return False
+    elif list_to_compare1[2:3] == list_to_compare2[2:3]:
+        return True
 
 
-def list_of_lists(list_of_lists_to_modify):
-    return "ANSWER HERE"  # Remove this line and implement
+
+
+def list_of_lists(listss):
+    return [listss[0][0:2]] + [listss[1][1:4]] + [listss[2][-2:]]
